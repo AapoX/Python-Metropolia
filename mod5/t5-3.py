7b5c35b7122c8467b1d902adc36c29c7ff68c8d5
AapoX <Aapo.tuomainen@gmail.com>
2023-09-08 13:40:33 +0300

    changes

--- mod5/t5-3.py
+++ mod5/t5-3.py
@@ -3,16 +3,14 @@ numcheck = False
 while True:
     num = int(input("Anna luku: "))
     if num == 1:
-        print(num, "Ei ole alkuluku")
+        print(num, "On alkuluku")
     elif num > 1:
         for i in range (2, num):
-            if (num % i) == 0:
+            if num % i == 0:
                 numcheck = True
                 break
             
-        if numcheck:
-            print(num, "ei ole alkuluku")
-            break
-        else:
-            print(num, "on alkuluku")
-            break
+    if numcheck:
+        print(num, "ei ole alkuluku")
+    else:
+        print(num, "on alkuluku")
